0ef5236473dabcbcb7c855421b58e0db9e9d87e4
No Hannah <67176677+nohannah@users.noreply.github.com>
2022-11-15 14:11:49 +0700

    About us

---
+++ About us/us.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/>
+  
+  <link rel="stylesheet" href="style.css">
+  <title>About us</title>
+</head>
+<body>
+<!-- Navbar-->
+<nav class="navbar navbar-expand-lg navbar-light bg-light py-5 fixed-top">
+  <div class="container">
+    <img class="logo" src="isi-logo.png"/>
+    
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+   
+    <h3> ISI IT Department</h3>
+      
+    </div>
+  </div>
+</nav>
+<br><br><br><br><br><br><br><br>
+<img class="img" src="White & Grey Minimalist Company Structure Organizational Chart.jpg">
+<br><br><br>
+  <!--footer-->
+  <footer class="'mt-5 py-5">
+    <div class="row container mx-auto pt-5">
+      <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+        <p class="pt-3"> We are IT Internship . We develop and customize the room booking in ISI TOWER.</p>
+      </div>
+      
+      <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+        <h5 class="pb-2">Contact</h5>
+      <div >
+        <h6 class="text-uppercase">Address: ISI Tower</h6>
+      </div>
+      <div >
+        <h6 class="text-uppercase">phone number : 098323722</h6>
+      </div>
+      <div >
+        <h6 class="text-uppercase">Email: nohannah508@gmail.com</h6>
+      </div>
+      </div>
+      <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+        <h5 class="pb-2">Our Mentor</h5>
+        <div class="row">
+          <img src="photo1667463658.jpeg" class="img-fluid w-25 h-100 m-2">
+          <img src="avatar711192846.jpg" class="img-fluid w-25 h-100 m-2">
+        </div>
+      </div>
+      <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+        <h5 class="pb-2">Us</h5>
+        <div class="row">
+          
+          <img src="avatar942410820.jpg" class="img-fluid w-25 h-100 m-2">
+          <img src="photo1667382180.jpeg" class="img-fluid w-25 h-100 m-2">
+          <img src="photo1667463588.jpeg" class="img-fluid w-25 h-100 m-2">
+        </div>
+      </div>
+    </div>
+    <div class="copyright mt-5">
+      <div class="row container mx-auto">
+        <div class="col-lg-3 col-md-5 col-sm-12 mb-4">
+        </div>
+        <div class="col-lg-3 col-md-5 col-sm-12 mb-4">
+          <p>ecommerce @ IT Team all right reserved</p>
+        </div>
+        <div class="col-lg-3 col-md-5 col-sm-12 mb-4 text-nowrap mb-2">
+          <a class="#"><i class="fab fa-facebook"></i></a>
+          <a class="#"><i class="fab fa-instagram"></i></a>
+          <a class="#"><i class="fab fa-twitter"></i></a>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  
+</body>
+</html>
